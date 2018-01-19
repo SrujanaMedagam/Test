c2fadf27c21564d5fddb3e31925ca4d1c860fc61
smedagam <smedagam@SHVLSP1485F1D.SLS.ads.valuelabs.net>
2018-01-19 19:21:40 +0530

    teat1

--- CoreJava/src/b.java
+++ CoreJava/src/b.java
@@ -3,6 +3,7 @@ public class b extends a {
 	void m2(){
 		System.out.println("m1 from b");
 		System.out.println("Srujana");
+		System.out.println("Srujana123");
 	}
 	public static void main(String args[]){
 		a b=new b();
