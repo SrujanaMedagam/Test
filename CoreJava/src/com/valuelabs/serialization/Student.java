8f2d49de5adcb7dc7683c8966ff99693d16590c9
smedagam <smedagam@SHVLSP1485F1D.SLS.ads.valuelabs.net>
2018-02-07 18:28:06 +0530

    keerthi

--- CoreJava/src/com/valuelabs/serialization/Student.java
+++ CoreJava/src/com/valuelabs/serialization/Student.java
@@ -24,6 +24,7 @@ public class Student  implements Serializable{
 		
 		super();
 		System.out.println("srujana");
+		System.out.println("Keerthi");
 		this.id = id;
 		this.name = name;
 		this.contact = contact;
